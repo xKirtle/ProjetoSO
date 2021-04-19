5d44b95173fd242df25a05ed3689b5cfa31a6974
xKirtle <xkirtle26@gmail.com>
2021-04-20 00:34:12 +0100

    S3

--- servidor.c
+++ servidor.c
@@ -7,10 +7,13 @@
  **
  **
  ******************************************************************************/
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 #include "common.h"
 
 Vaga vagas[NUM_VAGAS];
-Enfermeiro enfermeiros;
+Enfermeiro* enfermeiros;
 
 void registarServidor()
 {
@@ -24,7 +27,34 @@ void registarServidor()
     fclose(servidor);
 }
 
+void lerEnfermeiros()
+{
+    Enfermeiro enf[sizeof(Enfermeiro)];
+    FILE *enfData = fopen(FILE_ENFERMEIROS, "rb");
+    if (enfData != NULL)
+    {
+        fseek(enfData, 0, SEEK_END );
+        long fsize = ftell(enfData);
+        int nr_enf = fsize / sizeof(Enfermeiro);
+        enfermeiros = (Enfermeiro *)malloc(nr_enf * sizeof(Enfermeiro));
+
+        for (int i = 0; i < nr_enf; i++)
+            if (fread(enf, sizeof(Enfermeiro), 1, enfData) == 1)
+                enfermeiros[i] = *enf;
+
+        free(enfermeiros);
+
+        sucesso("S2) Ficheiro %s tem %ld bytes, ou seja, %d enfermeiros", FILE_ENFERMEIROS, fsize, nr_enf);
+
+        for (int i = 0; i < nr_enf; i++)
+            vagas[i].index_enfermeiro = -1;
+        sucesso("S3) Iniciei a lista de %d vagas", NUM_VAGAS);
+    }
+    else erro("S2) Não consegui ler o ficheiro %s!", FILE_ENFERMEIROS);
+}
+
 int main() 
 {
     registarServidor();
+    lerEnfermeiros();
 }
